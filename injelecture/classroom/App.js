06dc22c56d04818584fb7282b746f597e9ec4a18
Dark-jin <chch4lee1@naver.com>
2022-07-30 14:27:04 +0900

    7/30 지도화면 표시

--- injelecture/classroom/App.js
+++ injelecture/classroom/App.js
@@ -1,10 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Locations from './Locations';
 
-export default function App() {
+const Home = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.bigtext}>Hello Inje Lecture!</Text>
+      <Text style={styles.bigtext}>Inje Home</Text>
+      <Button
+        title="Go to 인제대학교"
+        onPress={()=>navigation.navigate('Locations')}
+      />
     </View>
+  )
+}
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Locations" component={Locations}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -16,8 +37,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   bigtext: {
-    alignContent:'center',
-    justifyContent: 'center',
-    fontSize: 45,
+    fontSize:45,
   },
 });
+export default App;
+
