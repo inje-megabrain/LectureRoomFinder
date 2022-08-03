6c3dc3858c8a1736b076252b3bc3587da561e2d0
MoonSangWon <kuim75@naver.com>
2022-08-03 16:48:57 +0900

    back properties divided & Swagger setting

--- summer/src/test/java/project_MJ/summer/SummerApplicationTests.java
+++ summer/src/test/java/project_MJ/summer/SummerApplicationTests.java
@@ -2,7 +2,9 @@ package project_MJ.summer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(locations = "/application.properties")
 @SpringBootTest
 class SummerApplicationTests {
 
